2156e8b7ed528cbcc92cbdab2bb0c6a163815478
mmmihaeel <101948402+mmmihaeel@users.noreply.github.com>
2022-05-18 19:03:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,131 @@
+$(document).ready(function () {
+    $('.slider__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider.icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider.icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: false,
+                arrows: false,
+            }
+        },]
+    });
+
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-card__content').eq(i).toggleClass('catalog-card__content_active')
+                $('.catalog-card__list').eq(i).toggleClass('catalog-card__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-card__link');
+    toggleSlide('.catalog-card__back');
+
+
+    //modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button__mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__description').text($('.catalog-card__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                }
+            },
+            messages: {
+                name: {
+                    required: "Будь ласка, введіть ваше ім'я",
+                    minlength: jQuery.validator.format(" Введіть {0} символів!")
+                },
+                phone: "Будь ласка, введіть ваш номер телефону",
+                email: {
+                    required: "Будь ласка, введіть вашу електронну адресу",
+                    email: "Неправильно введена електронна адреса",
+                },
+            },
+
+        });
+
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name="phone"]').mask("+(380)99-999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        };
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //smooth scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.page_up').fadeIn();
+        } else {
+            $('.page_up').fadeOut();
+        }
+    });
+    $("a[href^='#up']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
+    new WOW().init();
+});
+
+//const slider = tns({
+  //  container: '.slider__inner',
+  //  items: 1,
+  //  slideBy: 'page',
+  //  autoplay: false,
+  //  controls: false,
+  //  nav: false,
+
+//})
+//document.querySelector('button.prev').addEventListener('click', function () { slider.goTo('.prev') });
+//document.querySelector('button.next').addEventListener('click', function () { slider.goTo('.next') });
